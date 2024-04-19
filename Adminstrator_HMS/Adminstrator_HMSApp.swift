b9e267bd56ade4800ee97343045e669a340257ef
Aditya Pandey <anshu61796@gmail.com>
2024-04-19 14:47:26 +0530

    Initial Commit

---
+++ Adminstrator_HMS/Adminstrator_HMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adminstrator_HMSApp.swift
+//  Adminstrator_HMS
+//
+//  Created by Aditya Pandey on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Adminstrator_HMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
