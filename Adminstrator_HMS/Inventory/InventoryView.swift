4d79a85134a1c25d50d0324eaff4e3d01c6db89d
KAPAGANTI PARDHA SAI <kpardhasai2004@gmail.com>
2024-05-09 17:44:27 +0530

    added ui for inventory

--- Adminstrator_HMS/Inventory/InventoryView.swift
+++ Adminstrator_HMS/Inventory/InventoryView.swift
@@ -9,41 +9,85 @@ struct InventoryView: View {
     @ObservedObject var viewModel = InventoryViewModel()
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Blood Inventory")
-                .bold()
-                .font(.title)
-            List {
-                Section(header: Text("Blood Group          Quantity              Capacity")){
-                    ForEach(viewModel.bloodItems) { blood in
-                        Text("\(blood.id)                         \(blood.quantity)                          350")
-                            .multilineTextAlignment(.center)
+        NavigationStack {
+            VStack(spacing: 20) {
+                
+                VStack(spacing: 10) {
+                    HStack{
+                        Text("Oxygen Inventory")
+                            .font(.title2)
+                        Spacer()
+                        NavigationLink(destination: InventoryManagementView()) {
+                            
+                            Image(systemName: "square.and.pencil")
+                                .foregroundColor(.accentColor1)
+
+                            Text("Update")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .padding()
+                                .cornerRadius(10)
+                                .foregroundColor(.accentColor1)
+                        }
                     }
                     
-                }
-            }
-            Spacer()
-            
-            Text("Oxygen Inventory")
-//                .padding(.top)
-//                .padding(.top, 5)
-                .bold()
-                .font(.title)
-            List {
-                Section(header: Text("Number of Tanks")) {
-                    ForEach(viewModel.oxygenTanks) { tank in
-                        Text("Quantity                \(tank.quantity)")
+                    List {
+                        Section(header: Text("Number of Tanks")) {
+                            ForEach(viewModel.oxygenTanks) { tank in
+                                Text("Quantity                \(tank.quantity)")
+                            }
+                        }
                     }
+                    .frame(height: 100)
+                }
+                
+                VStack(spacing: 10) {
+                    HStack {
+                        Text("Blood Inventory")
+                            .font(.title2)
+                        Spacer()
+                    }
+                    
+                    List {
+                        Section(header:
+                            HStack {
+                                Text("Blood Group")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Spacer()
+                                Text("Quantity")
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                                Spacer()
+                                Text("Capacity")
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                            }
+                        ) {
+                            ForEach(viewModel.bloodItems) { blood in
+                                HStack {
+                                    Text(blood.id)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                    Spacer()
+                                    Text("\(blood.quantity)")
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                    Spacer()
+                                    Text("350")
+                                        .frame(maxWidth: .infinity, alignment: .trailing)
+                                }
+                                .padding(.vertical, 8)
+                            }
+                        }
+                    }
+                    .listStyle(GroupedListStyle())
+                    .cornerRadius(10)
                 }
             }
-            .frame(height: 130)
-        }
-        .onAppear {
-            viewModel.fetchInventory() {}
+            .padding()
+            .onAppear {
+                viewModel.fetchInventory() {}
+            }
         }
     }
 }
+
 
 
 struct InventoryView_Previews: PreviewProvider {
